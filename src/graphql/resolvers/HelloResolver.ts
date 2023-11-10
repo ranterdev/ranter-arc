947e9369285155ffe84a4c685004f9775fafd76f
spantheslayer <supriyosarkar282@gmail.com>
2023-11-10 16:48:12 +0530

    feat: basic skeleton ready 🚀

---
+++ src/graphql/resolvers/HelloResolver.ts
@@ -0,0 +1,15 @@
+import { Query, Resolver } from 'type-graphql'
+
+import HelloEntity from '@graphql/types/HelloTypes'
+
+@Resolver()
+class HelloResolver {
+    @Query(() => HelloEntity)
+    async hello(): Promise<HelloEntity> {
+        return {
+            test: 'Ola! Ranter 👋🏻'
+        }
+    }
+}
+
+export default HelloResolver
