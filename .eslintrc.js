477745cf497d07ef6fa425630e5115eaf918e5db
spantheslayer <supriyosarkar282@gmail.com>
2023-11-10 23:29:50 +0530

    chore: lint fix
    Signed-off-by: spantheslayer <supriyosarkar282@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,23 @@
 module.exports = {
-    env: {
-        es2021: true,
-        node: true
-    },
-    extends: [
-        'airbnb-base',
-        'airbnb-typescript/base',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended'
-    ],
-    overrides: [],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        project: './tsconfig.json'
-    },
-    plugins: ['@typescript-eslint', 'prettier'],
-    rules: {
-        'class-methods-use-this': 'off'
-    }
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'class-methods-use-this': 'off'
+  }
 }
